961de326667c619682732cb1c1b8a6e4f7068412
Paul Saxe <paul@thesaxes.net>
2019-05-31 15:28:13 -0400

    Still setting things up

--- setup.py
+++ setup.py
@@ -12,16 +12,16 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'numpy',
-    'pandas',
+    'numpy>=1.16.4',
+    'pandas>=0.24.2',
 ]
 
 setup_requirements = [
-    'pytest-runner',
+    'pytest-runner>=5.1',
 ]
 
 test_requirements = [
-    'pytest',
+    'pytest>=4.5.0',
 ]
 
 setup(
@@ -52,6 +52,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
     tests_require=test_requirements,
