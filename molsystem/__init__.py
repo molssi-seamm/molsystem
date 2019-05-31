01d9770dd6ec9ef96303f5cdfb03054fe3da6626
Paul Saxe <paul@thesaxes.net>
2019-05-31 19:20:27 -0400

    Bump version: 0.1.0 → 0.1.0

--- molsystem/__init__.py
+++ molsystem/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Paul Saxe"""
 __email__ = 'psaxe@vt.edu'
-__version__ = '0.1.0'
+__version__ = '__version__ = '0.1.0''
 
 # Bring up the classes so that they appear to be directly in
 # the molsystem package.
