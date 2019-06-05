b3eacf4059dd0a3343b22bfe03fcbd2968aa9600
Paul Saxe <paul@thesaxes.net>
2019-06-05 16:40:45 -0400

    Switching the sphinx.ext.napoleon, now that I realize that it exists.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ import molsystem
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode',
-              'sphinxcontrib.napoleon']
+              'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
