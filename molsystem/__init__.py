05167ca5450384b999b3420e03b293e2866efa96
Paul Saxe <psaxe@thesaxes.net>
2019-06-28 14:12:17 -0400

    Order is important! Table must come before Atoms.

--- molsystem/__init__.py
+++ molsystem/__init__.py
@@ -9,6 +9,6 @@ __version__ = '0.1.1'
 # Bring up the classes so that they appear to be directly in
 # the molsystem package.
 
+from molsystem.table import Table  # noqa: F401
 from molsystem.atoms import Atoms  # noqa: F401
 from molsystem.system import System  # noqa: F401
-from molsystem.table import Table  # noqa: F401
