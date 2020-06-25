256191b896ae87efa5dace6a2386d890249769b4
Paul Saxe <paul@thesaxes.net>
2020-06-25 11:12:22 -0400

    Changed to correctly handle versioneer.

--- molsystem/__init__.py
+++ molsystem/__init__.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
-"""Top-level package for MolSystem."""
-
-__author__ = """Paul Saxe"""
-__email__ = 'psaxe@vt.edu'
-__version__ = '0.1.2'
+"""
+molsystem
+A general implementation for molecular and periodic systems.
+"""
 
 # Bring up the classes so that they appear to be directly in
 # the molsystem package.
@@ -14,3 +13,12 @@ from molsystem.atoms import Atoms  # noqa: F401
 from molsystem.bonds import Bonds  # noqa: F401
 from molsystem.cell import Cell  # noqa: F401
 from molsystem.system import System  # noqa: F401
+
+# Handle versioneer
+from ._version import get_versions
+__author__ = """Paul Saxe"""
+__email__ = 'psaxe@molssi.org'
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
